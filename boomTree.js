7b713f94190b99d263ac1074553f9e7fa2728882
徐阳东 <xuyangdong@xuyangdongdeiMac.local>
2017-02-27 15:04:16 +0800

    boom tree

---
+++ boomTree.js
@@ -0,0 +1,39 @@
+function Node(data,left,right){
+  this.data = data
+  this.left = left
+  this.right = right
+}
+function show(){
+  console.log(this.data)
+}
+
+function insert(data){
+  let node = new Node(data,null,null)
+  if(this.root ==null){
+    this.root = node
+  }else{
+    let current = this.root
+    let parent = current
+    while(true){
+      if(current.data<data){
+        current = current.left
+        if(current = null){
+          parent.left = node
+          break
+        }
+      }else{
+        current = current.right
+        if(current = null){
+          parent.right = node
+          break
+        }
+      }
+    }
+  }
+}
+
+function BST(){
+  this.root = null
+  this.insert = insert
+  
+}
